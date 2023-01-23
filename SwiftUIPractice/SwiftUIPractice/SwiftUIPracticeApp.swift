0f1a1fa11a4a08992b1030b5ede9b2d9cc0ab1b9
orkunguneri <73347618+orkunguneri@users.noreply.github.com>
2023-01-23 20:15:18 +0300

    SwiftUIPractice

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by ORKUN GÜNERİ on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
